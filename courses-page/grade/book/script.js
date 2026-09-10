ee2f97a3eefb344ac64546b38c92eabd5c784d4b
王星霖 <wxl18760842104@163.com>
2026-09-10 11:13:22 +0800

    添加借阅数据定义

---
+++ courses-page/grade/book/script.js
@@ -0,0 +1,10 @@
+const records = [
+    { name: '张三', book: '西游记', days: 7 },
+    { name: '李四', book: '三国演义', days: 3 },
+    { name: '王五', book: '水浒传', days: 30 },
+    { name: '赵六', book: '红楼梦', days: 0 },
+    { name: '孙七', book: '百年孤独', days: 15 },
+    { name: '周八', book: '活着', days: 45 },
+    { name: '吴九', book: '平凡的世界', days: -2 }
+];
+
